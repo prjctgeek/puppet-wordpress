c557c351f8344ca79458d7010b7b53f010545087
Doug Chapman <prjctgeek@gmail.com>
2012-10-08 21:12:42 -0700

    switch to mysql db module removed db.pp add in multisite config bits quoted out

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,16 +11,34 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-import '*.pp'
 class wordpress(
-                $wordpress_db_name='wordpress',
-                $wordpress_db_user='wordpress',
-                $wordpress_db_password='password'
-                )
-{
-  $db_name = $wordpress_db_name
-  $db_user = $wordpress_db_user
-  $db_password = $wordpress_db_password
-  include wordpress::app
-  include wordpress::db
+        $db_name='wordpress',
+        $db_host='localhost',
+        $db_user = 'wordpress',
+        $db_password='ThereIsN0CowL3v3l')
+    {
+  class { 'wordpress::app': }
+
+  class { 'mysql::server':
+        config_hash => {
+            'root_password' => "Use0nlyinc4seof3merg3ncy",
+        },
+    }
+  class { 'mysql::server::account_security': }
+  database { "$db_name":
+        ensure          => present,
+        charset         => 'utf8',
+    }
+  database_user { "$db_user@%$db_host":
+        ensure          => present,
+        password_hash   => mysql_password("$db_password"),
+    } 
+  database_grant { "$db_user@$db_host/$db_name":
+        privileges      => [all],
+    }
+  class { 'mysql::backup':
+        backupuser      => 'backups',
+        backuppassword  => 'youD0haveAbackup-right?',
+        backupdir      => '/var/tmp',
+    }
 }
